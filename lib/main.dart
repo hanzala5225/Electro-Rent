7b443ac9c3889f4f5fd9d63d55f5b5652d21b9e9
Hanzala Ahmed Malik <hanzalaahmed6718@gmail.com>
2024-01-24 12:57:33 +0500

    Connected firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:electro_rent/firebase_options.dart';
 import 'package:electro_rent/screens/auth_ui/Sign_up_screen.dart';
 import 'package:electro_rent/screens/auth_ui/sign_in_screen.dart';
 import 'package:electro_rent/screens/auth_ui/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'screens/user_panel/main_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
