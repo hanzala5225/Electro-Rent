f6c2af76cb4e199005214a2d3c5c76f71caf4d13
Hanzala Ahmed Malik <hanzalaahmed6718@gmail.com>
2024-01-27 07:38:11 +0500

    Working on Sign Up Controller

--- lib/controllers/sign_up_controller.dart
+++ lib/controllers/sign_up_controller.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:electro_rent/models/user_models.dart';
 import 'package:electro_rent/utils/app_constant.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 
 class SignUpController extends GetxController{
@@ -21,12 +23,55 @@ Future<UserCredential?> signUpMethod(
     String userDeviceToken,
 
     ) async {
-  try{} on FirebaseAuthException catch(e) {
+  try{
+
+    EasyLoading.show(status: "Please Wait..");
+
+    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+      email: userEmail,
+      password: userPassword,
+    );
+
+    // send email verification
+    await userCredential.user!.sendEmailVerification();
+
+    UserModel userModel = UserModel(
+        uId: userCredential.user!.uid,
+        username: userName,
+        email: userEmail,
+        phone: userPhone,
+        userImg: '',
+        userDeviceToken: userDeviceToken,
+        country: '',
+        userAddress: '',
+        street: '',
+        isAdmin: false,
+        isActive: true,
+        createdOn: DateTime.now(),
+        city: userCity,
+    );
+
+    // add data into firebase
+
+    _firestore.collection('users')
+        .doc(userCredential.user!.uid)
+        .set(userModel.toMap()
+    );
+
+    EasyLoading.dismiss();
+
+    return userCredential;
+
+  } on FirebaseAuthException catch(e) {
+
+    EasyLoading.dismiss();
+
     Get.snackbar(
         "Error",
         "$e",
       snackPosition: SnackPosition.BOTTOM,
       backgroundColor: AppConstant.appSecondaryColor,
+      colorText: AppConstant.appTextColor,
     );
   }
 }
