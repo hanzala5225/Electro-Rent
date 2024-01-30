b79a441591ca70a256e65a5e626898c08afc3c4e
Hanzala Ahmed Malik <hanzalaahmed6718@gmail.com>
2024-01-30 14:57:13 +0500

    Created user with email & password firebase

---
+++ lib/controllers/Sign_in_controller.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:electro_rent/utils/app_constant.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+
+class SignInController extends GetxController{
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // for password visibility
+
+  var isPasswordVisible = false.obs;
+
+  Future<UserCredential?> signInMethod(
+      String userEmail,
+      String userPassword,
+      ) async {
+    try{
+
+      EasyLoading.show(status: "Please Wait..");
+
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: userEmail,
+        password: userPassword,
+      );
+      EasyLoading.dismiss();
+
+      return userCredential;
+
+    } on FirebaseAuthException catch(e) {
+
+      EasyLoading.dismiss();
+
+      Get.snackbar(
+        "Error",
+        "$e",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: AppConstant.appSecondaryColor,
+        colorText: AppConstant.appTextColor,
+      );
+    }
+  }
+}
